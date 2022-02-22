612b5745646873da35b50e787d9defb1e7176d5f
Joseph Hinckley <JoeyHinckley34@gmail.com>
2022-02-22 18:47:54 -0500

    added a button app

---
+++ practice/Button/Button/ButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonApp.swift
+//  Button
+//
+//  Created by Joey Hinckley on 2/22/22.
+//
+
+import SwiftUI
+
+@main
+struct ButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ practice/Button/Button/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Button
+//
+//  Created by Joey Hinckley on 2/22/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var ClickCount = 0
+    
+    var body: some View {
+        Button ( "Tap Count: \(ClickCount)"){
+            ClickCount += 1
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
