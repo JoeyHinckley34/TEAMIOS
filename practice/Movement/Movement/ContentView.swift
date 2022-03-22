61652bfe485b36ddc9818eee011f3b13695ab8ea
Joseph Hinckley <JoeyHinckley34@gmail.com>
2022-03-22 00:31:41 -0400

    Added relative positioning and a lives counter to movement

--- practice/Movement/Movement/ContentView.swift
+++ practice/Movement/Movement/ContentView.swift
@@ -9,52 +9,50 @@ import SwiftUI
 
 
 struct ContentView: View {
-    @State private var enemiePosition = CGPoint(x: 200, y:-10)
+    @State private var enemiePosition = CGPoint(x: UIScreen.main.bounds.width/2, y:-10)
+    
+    @State private var Lives = Double(20)
     
     let timer = Timer.publish(every: 0.2, on: .main, in: .common).autoconnect()
-    
     
     var body: some View {
         
         GeometryReader {   geo in
             ZStack{
-                
                 let NewNode = Node(position: enemiePosition, health: 10)
                 NodeView (node: NewNode)
                     .onReceive(self.timer){ _ in
                         self.moveEnemy()
                     }
-                
-                
             }
-            HStack(alignment: .bottom){
-                Text("\(enemiePosition.x) \(enemiePosition.y)")
+            VStack(alignment: .center){
+                Text("Enemie Position:")
+                Text("x:\( Double(enemiePosition.x).stringWithoutTrailingZeros) y: \( Double(enemiePosition.y).stringWithoutTrailingZeros)")
+                Text("Lives: \(Lives.stringWithoutTrailingZeros)")
             }
             .frame(width: geo.size.width, height: geo.size.width)
         }
         .edgesIgnoringSafeArea(.all)
         .background(Color.blue)
-        
-        
-//        .gesture(
-//            DragGesture(minimumDistance: 0)
-//                .onEnded{ drag in
-//                    let newNode = Node(position: drag.location, health: 10)
-//                    NodeView (node: newNode)
-//                }
-//        )
-        
+                
     }
     
     
     
     func moveEnemy(){
-        if(self.enemiePosition.y > 1100){
+        if(self.enemiePosition.y > UIScreen.main.bounds.height+10){
             self.enemiePosition.y = -10
+            Lives -= 1
         }
         withAnimation{
             self.enemiePosition.y += 30
         }
+    }
+}
+
+extension Double {
+    var stringWithoutTrailingZeros: String {
+        return truncatingRemainder(dividingBy: 1) == 0 ? String(format: "%.0f", self) : String(self)
     }
 }
 
