1f8c478df523c85ad5c5d156f9f008871cca0954
Yuerden <98784955+Yuerden@users.noreply.github.com>
2022-03-25 15:45:00 -0400

    Update walker.swift

--- DEV/walker.swift
+++ DEV/walker.swift
@@ -5,19 +5,12 @@ class walker: Enemy {
     #define ID 0
     #define REWARD 100
 
-    override var speed: Int  { get { return SPEED } }
-    override var hpMax: Int  { get { return MAX_HEALTH } }
-    override var ID: Int     { get { return ID } }
+    override init() { 
+        speed = SPEED
+        hpMax = MAX_HEALTH
+        id = ID
+        reward = REWARD
+        super.init()
+     }
 
-    override init() { init(x: 0, y: 0) }
-
-    override init(x: Int, y: Int){
-        super.init(x: x, y: y)
-    }
-
-    override func getReward() -> Int { 
-        if(isDead)
-            return REWARD
-        return 0
-    }
 }
