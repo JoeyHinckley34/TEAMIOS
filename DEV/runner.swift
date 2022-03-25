72385e9de1f5932d72da51f36d599fb8d49e483e
Yuerden <98784955+Yuerden@users.noreply.github.com>
2022-03-25 15:43:48 -0400

    Update runner.swift

--- DEV/runner.swift
+++ DEV/runner.swift
@@ -5,19 +5,11 @@ class runner: Enemy {
     #define ID 3
     #define REWARD 100
     
-    override var speed: Int  { get { return SPEED } }
-    override var hpMax: Int  { get { return MAX_HEALTH } }
-    override var ID: Int     { get { return ID } }
-
-    override init() { init(x: 0, y: 0) }
-
-    override init(x: Int, y: Int){
-        super.init(x: x, y: y, speed: SPEED, hpMax: MAX_HEALTH)
-    }
-
-    override func getReward() -> Int { 
-        if(isDead)
-            return REWARD
-        return 0
-    }
+    override init() { 
+        speed = SPEED
+        hpMax = MAX_HEALTH
+        id = ID
+        reward = REWARD
+        super.init()
+     }
 }
