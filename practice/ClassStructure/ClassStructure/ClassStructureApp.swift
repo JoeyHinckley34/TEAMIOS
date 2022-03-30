c562dc49f7479b941af15aafa7ecfbace1a13fb9
Andrew May <maya2@rpi.edu>
2022-03-29 22:10:07 -0400

    trying to follow class structure

---
+++ practice/ClassStructure/ClassStructure/ClassStructureApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClassStructureApp.swift
+//  ClassStructure
+//
+//  Created by administrator on 3/29/22.
+//
+
+import SwiftUI
+
+@main
+struct ClassStructureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ practice/ClassStructure/ClassStructure/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ClassStructure
+//
+//  Created by administrator on 3/29/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
