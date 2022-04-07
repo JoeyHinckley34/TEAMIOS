cf0692a75342529e543a6edf82dcf1f9c7a6bf12
Yuerden <98784955+Yuerden@users.noreply.github.com>
2022-04-07 16:10:59 -0400

    Update Helpers.swift

--- practice/TapForNewView/TapForNewView/Helpers.swift
+++ practice/TapForNewView/TapForNewView/Helpers.swift
@@ -21,30 +21,52 @@ func distance(_ a: CGPoint, _ b: CGPoint) -> CGFloat {
 func appendNewWave(initialEV: [EnemieView]) -> [EnemieView]{
     
     var eV:[EnemieView] = initialEV
-
-    eV.append(spawnEnemy(yOffset: -50))
-    eV.append(spawnEnemy(yOffset: -100))
-    eV.append(spawnEnemy(yOffset: -150))
+    
+    eV.append(spawnRunner(yOffset: 0))
+    eV.append(spawnSprinter(yOffset: -50))
+    eV.append(spawnWalker(yOffset: -100))
+    eV.append(spawnTank(yOffset: -150))
     
     return eV
 }
 
 //Spawns a single Enemy, appends to enemyViews array
 func spawnEnemy(yOffset: CGFloat) -> EnemieView{
-    let NewEnemy = Enemy(position: CGPoint(x:UIScreen.main.bounds.width/2, y: .zero-yOffset), health: 10)
+    let NewEnemy = Enemy(position: CGPoint(x:UIScreen.main.bounds.width/2, y: .zero-yOffset))
     let enemyView = EnemieView (enemy: NewEnemy)
     return enemyView
-    //enemyViews.append(Enemy(position: CGPoint(x: start.x, y: start.y-yOffset), health: 10))
+}
+func spawnWalker(yOffset: CGFloat) -> EnemieView{
+    let NewEnemy = Walker(position: CGPoint(x:UIScreen.main.bounds.width/2, y: .zero-yOffset))
+    let enemyView = EnemieView (enemy: NewEnemy)
+    return enemyView
+}
+func spawnTank(yOffset: CGFloat) -> EnemieView{
+    let NewEnemy = Tank(position: CGPoint(x:UIScreen.main.bounds.width/2, y: .zero-yOffset))
+    let enemyView = EnemieView (enemy: NewEnemy)
+    return enemyView
+}
+func spawnRunner(yOffset: CGFloat) -> EnemieView{
+    let NewEnemy = Runner(position: CGPoint(x:UIScreen.main.bounds.width/2, y: .zero-yOffset))
+    let enemyView = EnemieView (enemy: NewEnemy)
+    return enemyView
+}
+func spawnSprinter(yOffset: CGFloat) -> EnemieView{
+    let NewEnemy = Sprinter(position: CGPoint(x:UIScreen.main.bounds.width/2, y: .zero-yOffset))
+    let enemyView = EnemieView (enemy: NewEnemy)
+    return enemyView
 }
 
 
 //Updates Enemy in position and checks life to be filtered if dead
 //Returns updated/filtered enemy array
 //Ex of enemies being updated: "enemyViews = updateEnemies(enemyViewsArray: enemyViews)"
-func moveEnemies(enemyViewsArray: [EnemieView]) {
+func moveEnemies(enemyViewsArray: [EnemieView]) -> Double{
+    var lostLives:Double = 0
     for currentEnemy in enemyViewsArray {
         if(!currentEnemy.enemy.isDead){
-            currentEnemy.enemy.move()
+            lostLives += currentEnemy.enemy.move()
         }
     }
+    return lostLives
 }
