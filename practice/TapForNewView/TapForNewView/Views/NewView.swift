6b949aa2ee8e7eccf27a106aac44f74ea6264423
Yuerden <98784955+Yuerden@users.noreply.github.com>
2022-04-07 16:15:43 -0400

    Create NewView.swift

---
+++ practice/TapForNewView/TapForNewView/Views/NewView.swift
@@ -0,0 +1,15 @@
+//
+//  NewView.swift
+//  TapForNewView
+//
+//  Created by john cohen on 4/7/22.
+//
+
+import Foundation
+import SwiftUI
+
+struct NewView: Identifiable {
+    var id = UUID()
+    var tower: Tower
+    //var location:CGPoint
+}
