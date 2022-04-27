7dd43df421e9386ad84bd8d71c03f94d7e17f355
Yuerden <98784955+Yuerden@users.noreply.github.com>
2022-04-27 14:57:34 -0400

    Update DotsAndBoxesApp.swift

--- DotsAndBoxes/DotsAndBoxes/DotsAndBoxesApp.swift
+++ DotsAndBoxes/DotsAndBoxes/DotsAndBoxesApp.swift
@@ -8,20 +8,40 @@
 import SwiftUI
 
 // Main Game Driver
+
 @main
 struct DotsAndBoxesApp: App {
     @State var showGame = false
+    @State var lvl: Int = 0
     var body: some Scene {
         WindowGroup {
            Menu("Menu") {
-                Button("Start", action: start)
-                Button("Exit to menu", action: menu)
+               Button("Endless", action: endless)
+               Button("Level 1", action: lvl1)
+               Button("Level 2", action: lvl2)
+               Button("Level 3", action: lvl3)
+               Button("Exit to menu", action: menu)
             }
             if(showGame){
-                ContentView() // game engine
+                ContentView(lvl: lvl) // game engine
             }
         }
     }
-    func start() { showGame = true  }
-    func menu()  { showGame = false }
+    func endless()  {
+        showGame = true
+        lvl = 0
+    }
+    func lvl1()     {
+        showGame = true
+        lvl = 1
+    }
+    func lvl2()     {
+        showGame = true
+        lvl = 2
+    }
+    func lvl3()     {
+        showGame = true
+        lvl = 3
+    }
+    func menu()     { showGame = false }
 }
